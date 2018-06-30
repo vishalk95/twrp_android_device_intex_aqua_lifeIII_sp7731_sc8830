03e5031fbd6b369df5eb43f2f9a302a324dba2c9
vishalk17 <vishalkapadi95@gmail.com>
2018-06-30 12:54:39 +0000

    add stock modules

--- device.mk
+++ device.mk
@@ -5,3 +5,11 @@ DEVICE_PATH := device/intex/lifeIII
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/init.recovery.sc8830.rc:root/init.recovery.sc8830.rc \
     $(DEVICE_PATH)/rootdir/ueventd.sc8830.rc:root/ueventd.sc8830.rc
+
+# Stock Modules
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/lib/modules/autotst.ko:root/lib/modules/autotst.ko \
+    $(DEVICE_PATH)/rootdir/lib/modules/gator.ko:root/lib/modules/gator.ko \
+    $(DEVICE_PATH)/rootdir/lib/modules/mmc_test.ko:root/lib/modules/mmc_test.ko \
+    $(DEVICE_PATH)/rootdir/lib/modules/sprdwl.ko:root/lib/modules/sprdwl.ko \
+    $(DEVICE_PATH)/rootdir/lib/modules/trout_fm.ko:root/lib/modules/trout_fm.ko
